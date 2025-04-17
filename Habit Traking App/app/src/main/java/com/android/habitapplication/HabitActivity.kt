9e8304d0f3370560ed2a39d328767c27efcc8d0f
BasmaElsaqa <elsaqabasma@gmail.com>
2025-04-17 17:08:10 +0200

    Create Files For UI

---
+++ Habit Traking App/app/src/main/java/com/android/habitapplication/HabitActivity.kt
@@ -0,0 +1,20 @@
+package com.android.habitapplication
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class HabitActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_habit)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
