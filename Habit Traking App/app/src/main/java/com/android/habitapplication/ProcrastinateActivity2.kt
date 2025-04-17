f2acca38abd2b00a29b2a9502b146d79690fa00e
BasmaElsaqa <elsaqabasma@gmail.com>
2025-04-18 00:04:12 +0200

    4 Screens Added

--- Habit Traking App/app/src/main/java/com/android/habitapplication/ProcrastinateActivity2.kt
+++ Habit Traking App/app/src/main/java/com/android/habitapplication/ProcrastinateActivity2.kt
@@ -6,11 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class ProcrastieActivity : AppCompatActivity() {
+class ProcrastinateActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_procrastie)
+        setContentView(R.layout.activity_procrastinate2)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
